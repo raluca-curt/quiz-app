60cd92723a12e547d774c9ea68bd070aaf3c9671
Raluca Curt <ralucaioanacurt@gmail.com>
2022-12-21 22:40:32 +0200

    Implemented quiz with questions, choices, and correct answer

--- app.js
+++ app.js
@@ -2,39 +2,121 @@
 const MAX_QUESTIONS = 5;
 // Store current game's questions
 let questions = []
+let availableQuestions = [];
+let displayedQuestions = 0;
+let currentQuestion = 0;
+
+let choices = Array.from(document.querySelectorAll('.choice-text'));
 
 // Fetch questions
-fetch(`https://opentdb.com/api.php?amount=${MAX_QUESTIONS}`)
-    .then(res => res.json())
-    .then(fetchedQuestions => {
-        questions = fetchedQuestions.results;
-        // Go through every question in questions
-        questions.map(question => {
-            // Get the actual question
-            const formattedQuestion = {question: question.question, };
+document.addEventListener('DOMContentLoaded', () => {
+    getJSON = async () => {
+        return await fetch(`https://opentdb.com/api.php?amount=${MAX_QUESTIONS}&type=multiple`)
+            .then(res => res.json())
+            .then(fetchedQuestions => {
+                questions = fetchedQuestions.results.map(question => {
+                    // Get the actual question
+                    const formattedQuestion = {question: question.question, };
 
-            // Get the question's wrong answers
-            const formattedAnswers = [...question.incorrect_answers];
+                    // Get the question's wrong answers
+                    const formattedAnswers = [...question.incorrect_answers];
 
-            // Get the question's correct answer
-            const correct_answer = question.correct_answer;
+                    // Get the question's correct answer
+                    const correctAnswer = question.correct_answer;
 
-            // Get a number between 1 and 4 and append it to question in order to keep track of the correct answer
-            formattedQuestion.answer_index = Math.floor(Math.random() * 4) + 1;
+                    // Get a number between 1 and 4 and append it to question in order to keep track of the correct answer
+                    formattedQuestion.answerIndex = Math.floor(Math.random() * 4) + 1;
 
-            // Append the correct answer to formattedAnswers at the random index location
-            formattedAnswers.splice(formattedQuestion.answer_index - 1, 0, correct_answer);
+                    // Append the correct answer to formattedAnswers at the random index location
+                    formattedAnswers.splice(formattedQuestion.answerIndex - 1, 0, correctAnswer);
 
-            // Append answers to each question
-            formattedAnswers.forEach((choice, index) => {
-                formattedQuestion['choice' + (index+1)] = choice;
+                    // Append answers to each question
+                    formattedAnswers.forEach((choice, index) => {
+                        formattedQuestion['choice' + (index+1)] = choice;
+                    })
+                    
+                    return formattedQuestion;
+                    
+                })
+                // console.log(questions);
+                // questions = questions;
+                startGame();
             })
-            console.log(formattedQuestion);
-            return formattedQuestion;
+            .catch(err => console.log(err));
+    }
+    getJSON();
+    // once questions are fetched, display a random question from questions
+    // at the end delete current question from questions
+    startGame = () => {
+        availableQuestions = [...questions];
+        displayedQuestions = 0;
+
+        // Start the game by displaying a question
+        getQuestion();
+    }
+
+    getQuestion = () => {
+        // If this is the last question
+        if (currentQuestion >= MAX_QUESTIONS || availableQuestions.length === 0) {
+            return window.location.assign('./index.html');
+        } 
+
+        // Keep track of how many questions we've displayed so far
+        displayedQuestions++;
+
+        // Generate random index
+        const index = Math.floor(Math.random() * availableQuestions.length);
+
+        // Access the current question
+        currentQuestion = availableQuestions[index];
+
+        // Display the current question
+        document.getElementById('question').innerText = currentQuestion.question;
+        // console.log(availableQuestions);
+        
+        choices.forEach(choice => {
+            // Get the choice's id (ex: 1, 2, 3, 4)
+            const num = choice.id;
+
+            // Display each question choice 
+            choice.innerText = currentQuestion['choice' + num];
         })
-        // console.log(questions);
+
+        // Delete the used question from availableQuestion
+        availableQuestions.splice(index, 1);
+    }
+
+
+    // Record user's answer
+    choices.forEach(choice => {
+        choice.addEventListener('click', (e) => {
+            // Get user choice
+            const userChoice = e.target;
+
+            // Get user choice id
+            const userChoiceId = userChoice.id;
+
+            // Check if user choice id matches correct answer
+            const checkAnswer = (userChoiceId === currentQuestion.answerIndex) ? 'correct' : 'incorrect';
+
+            // Add class 'correct' or 'incorrect' to parent
+            userChoice.parentElement.classList.add(checkAnswer);
+
+            // Remove class and generate new q
+            setTimeout(() => {
+                userChoice.parentElement.classList.remove(checkAnswer);
+                getQuestion(); 
+            }, 1000)
+        })
     })
-    .catch(err => console.log(err));
+    
 
 
 
+
+
+
+
+
+
+})
